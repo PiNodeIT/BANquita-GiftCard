73e66f90408c755746e42ca5f5bf2fd3c840e043
PiNodeIT <95429416+PiNodeIT@users.noreply.github.com>
2021-12-02 16:23:52 +0100

    Create banquita-gift.py

---
+++ banquita-gift.py
@@ -0,0 +1,43 @@
+# Python Script for generate BANquita-themed BANANO GiftCard
+# Version 0.1
+# Author Monk3yBanano | BanaoPiNode 
+# Import QR Code ad Pillow library
+import qrcode
+from PIL import Image
+import os
+
+# Create qr code instance
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_H,
+    box_size = 10,
+    border = 1,
+)
+
+# The data that you want to store
+data = input("Insert Seed:\n")
+name = input("Save as (no extension) :\n")
+
+# Add data
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image()
+qr_img = img.resize((80,80))
+
+# Temp Save QRcode
+qr_img.save((name)+".png")
+
+# Merge QRCode on BG Image
+bg = Image.open('./src/bg.png')
+qr_code = Image.open((name)+".png")
+cp_bg = bg.copy()
+cp_bg.paste(qr_code, (344, 5))
+
+# Save GiftCard on "GiftCard" folder
+cp_bg.save("./GiftCard/"+(name)+"_GiftCard.png", quality=95)
+
+# Remove Temp QRCode file
+os.remove((name)+".png")
+
