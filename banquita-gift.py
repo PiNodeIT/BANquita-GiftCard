6c0d5d5592f3671c23358d84a2d6c36dc9ff3294
PiNodeIT <95429416+PiNodeIT@users.noreply.github.com>
2021-12-03 12:55:37 +0100

    Add fuction to save the deposit address QR
    Now the script make 2 file 
    -  (name)__GiftCard.png is the giftcard
    - (name)__Deposit.png is the deposit address qrcode (to put some BAN in the giftcard)

--- banquita-gift.py
+++ banquita-gift.py
@@ -1,6 +1,7 @@
 # Python Script for generate BANquita-themed BANANO GiftCard
 # Version 0.1
-# Author Monk3yBanano | BanaoPiNode 
+# Author Monk3yBanano | BanaoPiNode
+
 # Import QR Code ad Pillow library
 import qrcode
 from PIL import Image
@@ -16,6 +17,7 @@ qr = qrcode.QRCode(
 
 # The data that you want to store
 data = input("Insert Seed:\n")
+deposit = input("Insert Deposit Address (must start in ban_ ):\n")
 name = input("Save as (no extension) :\n")
 
 # Add data
@@ -41,3 +43,21 @@ cp_bg.save("./GiftCard/"+(name)+"_GiftCard.png", quality=95)
 # Remove Temp QRCode file
 os.remove((name)+".png")
 
+# Create qr code instance
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_H,
+    box_size = 10,
+    border = 1,
+)
+
+# Add data deposit address
+qr.add_data(deposit)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+dep = qr.make_image()
+dep_img = dep.resize((300,300))
+
+#Save Deposit QRcode
+dep_img.save("./GiftCard/"+(name)+"_DEPOSIT.png", quality=95)
